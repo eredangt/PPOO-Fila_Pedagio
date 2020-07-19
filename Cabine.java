df76fccbea2f45d6706899d59dc6a7f5ddd7ff68
LuizFelipe-5 <luiz.silva16@estudante.ufla.br>
2020-07-19 19:15:34 -0300

    Criação método ehAutomatico

--- Cabine.java
+++ Cabine.java
@@ -2,7 +2,7 @@
 ---------------------------------------------------------------------------------
 Trabalho Prático - Práticas de Programação Orientada a Objetos - GCC178 - 2020/01
 ----------------Grupo 05 - Fila de veículos em pedágio rodoviário----------------
-    Integrantes: 
+    Integrantes:
         Caio de Oliveira (10A - 201820267),
         Ismael Martins Silva (10A - 201820281),
         Layse Cristina Silva Garcia (10A - 201811177),
@@ -16,7 +16,7 @@ import java.util.NoSuchElementException;
 
 /**
 * Classe que representa a Cabine em um pedágio.
-* Ela gera um Identificador único para cada cabine criada e 
+* Ela gera um Identificador único para cada cabine criada e
 * fornece métodos para a manipulação de seus atributos.
 * Uma cabine utiliza apenas um tipo de atendimento.
 */
@@ -24,18 +24,31 @@ public class Cabine {
 	private static int numeroCabines = 0;
 	private int idCabine;
 	private Queue<Veiculo> filaVeiculos;
+	private Atendimento atendimento;
 
 	/**
-	* Construtor incrementa o contador de Cabines e 
+	* Construtor incrementa o contador de Cabines e
 	* define os IDs de cada Cabine a partir do valor obtido.
 	* O construtor é responsável por criar uma fila de Veículos.
+	* @param atendimento - objeto que representa um tipo de
+	* atendimento.
 	*/
-	public Cabine() {
+	public Cabine(Atendimento atendimento) {
 		numeroCabines += 1;
 		idCabine = numeroCabines;
 		filaVeiculos = new LinkedList<Veiculo>();
+		this.atendimento = atendimento;
 	}
-	
+
+	/**
+	* Método que informa se uma cabine é automatica.
+	* @return boolean - true se o atendimento da cabine for
+	* automatico.
+	*/
+	public boolean ehAutomatico() {
+		return atendimento instanceof Automatico;
+	}
+
 	/**
 	* Método que informa quantos objetos da classe Cabine
 	* foram instanciados.
@@ -45,7 +58,7 @@ public class Cabine {
 	public int getNumeroCabines() {
 		return numeroCabines;
 	}
-	
+
 	/**
 	* Método que informa o identificador único de uma cabine.
 	* @return int - contendo o número que representa o ID da cabine.
@@ -65,7 +78,7 @@ public class Cabine {
 	* Caso ocorra a tentativa de retirar um veículo na fila vazia,
 	* uma mensagem de erro é retornada.
 	* @return Veiculo - remove o Veículo da fila com sucesso.
-	* @return NoSuchElementException - mensagem de erro ao remover 
+	* @return NoSuchElementException - mensagem de erro ao remover
 	* um objeto em uma fila vazia.
 	*/
 	public Veiculo desenfileirarVeiculo() {
@@ -76,7 +89,7 @@ public class Cabine {
 			throw e;
 		}
 	}
-	
+
 	/**
 	* Método que calcula o tamanho da fila de veículos no pedágio.
 	* @return int - contendo a quantidade de veículos na fila.
@@ -84,7 +97,7 @@ public class Cabine {
 	public int calcularTamanho() {
 		return filaVeiculos.size();
 	}
-	
+
 	/**
 	* Método utilizado apenas para fins de debug.
 	* @return String - uma cadeia de caracteres formatada
@@ -94,12 +107,12 @@ public class Cabine {
 		String dadosCompletos = "";
 		dadosCompletos = String.format("Número da Cabine: %s\n", getIdCabine());
 		dadosCompletos += "--------------------\n";
-		
+
 		for (Veiculo v : filaVeiculos) {
 			dadosCompletos += v.toString();
 			dadosCompletos += "--------------------\n";
 		}
-		
+
 		return dadosCompletos;
-	} 
+	}
 }
