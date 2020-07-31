1e8e179ab06c1e3a7ddf906e82bf4afdc2694c62
LuizFelipe-5 <luiz.silva16@estudante.ufla.br>
2020-07-31 17:32:30 -0300

    organizacao metodos (Simulador)

--- Simulador.java
+++ Simulador.java
@@ -129,86 +129,92 @@ public class Simulador {
     }
 
     /**
-    * Método que executa a Simulação.
-    * Enquanto minha fila de Eventos não for vazia, realizo a retirada dos
-    * eventos na fila de Evento.
+    * Método que calcula o tempo gasto na execução.
+    * @return long - retorna o tempo gasto na execução.
     */
-    private void executarSimulacao() {
-        int tempoUltimoEvento = -1;
-        int tempoExecucao = 0;
-        Evento eventoAtual;
-
-        do {
-            eventoAtual = filaEventos.peek();
-
-            if (eventoAtual.getTempoEvento() == tempoExecucao) {
-                eventoAtual = desenfilerarEvento();
-                tempoUltimoEvento = eventoAtual.getTempoEvento();
-
-                try {
-                    executarEvento(eventoAtual);
-                }
-                catch (Exception e) {
-                    throw new RuntimeException("Erro na execução de um evento!\n" + e.getMessage());
-                }
-            }
-            tempoExecucao ++;
-            atualizaMediaFilas();
-        }
-        while (!filaEventos.isEmpty());
-
-        if (tempoUltimoEvento != -1) {
-            setTempoTotalSimulado(tempoUltimoEvento);
-        }
-
-        System.out.println(gerarEstatisticas());
+    public long calcularTempoExecucao() {
+        return 0L;
     }
 
     /**
-    * Método que enfileira Chegadas.
-    * Caso seja a Simulação por Fila Aleatória, os eventos são adicionados
-    * a partir dessa ideia.
-    * Caso a Simulação seja sem ter Fila Aleatória, então adiciono os eventos
-    * de acordo com o valor falso do atirbuto filaRand.
+    * Método que gera as Estatisticas da fila de Pegágio.
+    * @return String - retorna a saída final da fila.
     */
-    private void enfileirarChegadas() {
-        int tempoChegada = 0;
-        ArrayList<Integer> listaTemposUsados = new ArrayList<Integer>();
+    public String gerarEstatisticas() {
+        String texto;
+        String textoMaioresTamanhos = getTextoTamanhos("Maior");
+        String textoMedioTamanhos = getTextoTamanhos("Medio");
+        String textoMedioAtendimentosTotal = getTextoMedioAtendimentos("Total");
+        String textoMedioAtendimentosEspecífico = "";
+        textoMedioAtendimentosEspecífico += getTextoMedioAtendimentos("Leve");
+        textoMedioAtendimentosEspecífico += getTextoMedioAtendimentos("Pesado");
 
-        for (Map.Entry<Integer, Veiculo> veiculo : veiculos.entrySet()) {
-            Veiculo veiculoAtual = veiculo.getValue();
+        texto = String.format("Tempo total de simulacao: %d\n\n", getTempoTotalSimulado()) +
+                String.format("Numero de eventos tradados: %d\n\n", getNumeroEventosTratados()) +
+                String.format("Tempos médios de espera em cada fila de atendimento:\n\n%s", textoMedioAtendimentosTotal) +
+                String.format("Tamanho médio das filas de atendimento:\n\n%s", textoMedioTamanhos) +
+                String.format("Tamanho máximo das filas de atendimento:\n\n%s", textoMaioresTamanhos) +
+                String.format("Tempo médio de atendimento de cada tipo de cliente:\n\n%s", textoMedioAtendimentosEspecífico);
 
-            Random random = new Random();
-            tempoChegada += random.nextInt(getIntervaloChegada());
-
-            while (listaTemposUsados.contains(tempoChegada)) {
-                tempoChegada += 1;
-            }
-            listaTemposUsados.add(tempoChegada);
-
-            temposEventos.add(tempoChegada);
-            Chegada eventoAtual = new Chegada(tempoChegada, veiculoAtual.getIdVeiculo());
-            filaEventos.add(eventoAtual);
-        }
+        return texto;
     }
 
-    private void atualizaMediaFilas() {
+    public String getTextoMedioAtendimentos(String abordagem) {
+        String texto = "";
+        int idCabineAtual = 0;
+        Cabine cabineAtual = null;
+        int tempo = 0;
+
+        if (abordagem.equals("Leve")) {
+            texto += "Média de tempo de espera de Veículos Leves:\n";
+        }
+        else if (abordagem.equals("Pesado")) {
+            texto += "Média de tempo de espera de Veículos Pesados:\n";
+        }
+
         for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
-            Cabine cabineAtual = cabine.getValue();
-            cabineAtual.adicionaNovoTamanho(cabineAtual.calcularTamanho());
+            idCabineAtual = cabine.getKey();
+            tempo = getTempoMedioEsperaGeral(idCabineAtual, abordagem);
+
+            texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tempo);
         }
+        return texto + "\n";
     }
 
-    private void setTempoTotalSimulado(int tempo) {
-        tempoTotalSimulado = tempo;
+    public String getTextoTamanhos(String abordagem) {
+        String texto = "";
+        int idCabineAtual = 0;
+        int tamanho = 0;
+
+        if (abordagem.equals("Maior")) {
+            for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
+                idCabineAtual = cabine.getKey();
+                tamanho = getTamanhoMaiorFila(idCabineAtual);
+
+                texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tamanho);
+            }
+        }
+        else { // if (abordagem.equals("Medio") {
+            for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
+                idCabineAtual = cabine.getKey();
+                tamanho = getTamanhoMedioFilaGeral(idCabineAtual);
+
+                texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tamanho);
+            }
+        }
+        return texto + "\n";
     }
 
     private int getTempoTotalSimulado() {
         return tempoTotalSimulado;
     }
 
-    private void incrementaNumeroEventos() {
-        numeroEventosTratados += 1;
+    /**
+    * Método que informa o intervalo de chegada.
+    * @return int - reotrna o valor do intervaloChegada.
+    */
+    public int getIntervaloChegada() {
+        return intervaloChegada;
     }
 
     private int getNumeroEventosTratados() {
@@ -286,20 +292,56 @@ public class Simulador {
         }
     }
 
-    /**
-    * Método que informa o intervalo de chegada.
-    * @return int - reotrna o valor do intervaloChegada.
-    */
-    public int getIntervaloChegada() {
-        return intervaloChegada;
+    private Cabine getCabine(int idCabine) {
+        return cabines.get(idCabine);
+    }
+
+    private Atendimento getAtendimento(int idAtendimento) {
+        return atendimentos.get(idAtendimento);
+    }
+
+    private Veiculo getVeiculo(int idVeiculo) {
+        return veiculos.get(idVeiculo);
+    }
+
+    private void incrementaNumeroEventos() {
+        numeroEventosTratados += 1;
     }
 
     /**
-    * Método que retira um Evento da fila.
-    * @return Evento - remove o Evento da fila com sucesso.
+    * Método que executa a Simulação.
+    * Enquanto minha fila de Eventos não for vazia, realizo a retirada dos
+    * eventos na fila de Evento.
     */
-    private Evento desenfilerarEvento() {
-        return filaEventos.remove();
+    private void executarSimulacao() {
+        int tempoUltimoEvento = -1;
+        int tempoExecucao = 0;
+        Evento eventoAtual;
+
+        do {
+            eventoAtual = filaEventos.peek();
+
+            if (eventoAtual.getTempoEvento() == tempoExecucao) {
+                eventoAtual = desenfilerarEvento();
+                tempoUltimoEvento = eventoAtual.getTempoEvento();
+
+                try {
+                    executarEvento(eventoAtual);
+                }
+                catch (Exception e) {
+                    throw new RuntimeException("Erro na execução de um evento!\n" + e.getMessage());
+                }
+            }
+            tempoExecucao ++;
+            atualizaMediaFilas();
+        }
+        while (!filaEventos.isEmpty());
+
+        if (tempoUltimoEvento != -1) {
+            setTempoTotalSimulado(tempoUltimoEvento);
+        }
+
+        System.out.println(gerarEstatisticas());
     }
 
     /**
@@ -323,17 +365,6 @@ public class Simulador {
         catch (Exception e) {
             throw new RuntimeException("Erro na execução da chegada ou saída.\n" + e.getMessage());
         }
-    }
-
-    private void enfileirarNaCabine(int idCabine, int idVeiculo) {
-        Cabine cabineAtual = getCabine(idCabine);
-        cabineAtual.enfileirarVeiculo(idVeiculo);
-    }
-
-    private void enfileirarSaida(int tempoChegada, int idCabine) {
-        temposEventos.add(tempoChegada);
-        filaEventos.add(new Saida(tempoChegada, idCabine));
-
     }
 
     /**
@@ -372,16 +403,83 @@ public class Simulador {
         }
     }
 
-    private Cabine getCabine(int idCabine) {
-        return cabines.get(idCabine);
+    /**
+    * Método que executa a Saída.
+    * Assim que executo o método, retiro o elemento da fila.
+    * @param e - evento de saida.
+    */
+    private int executarSaida(Saida eventoAtual) {
+        int idVeiculoRemovido = -1;
+        try {
+            Cabine cabineParaRemover = getCabine(eventoAtual.getIdCabine());
+            idVeiculoRemovido = cabineParaRemover.desenfileirarVeiculo();
+            Veiculo veiculoRemovido = getVeiculo(idVeiculoRemovido);
+
+            String tipoVeiculo = (veiculoRemovido instanceof VeiculoLeve) ? "Leve" : "Pesado";
+            cabineParaRemover.armazenaTempo(tipoVeiculo, veiculoRemovido.getTempoEspera());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return idVeiculoRemovido;
     }
 
-    private Atendimento getAtendimento(int idAtendimento) {
-        return atendimentos.get(idAtendimento);
+    /**
+    * Método que enfileira Chegadas.
+    * Caso seja a Simulação por Fila Aleatória, os eventos são adicionados
+    * a partir dessa ideia.
+    * Caso a Simulação seja sem ter Fila Aleatória, então adiciono os eventos
+    * de acordo com o valor falso do atirbuto filaRand.
+    */
+    private void enfileirarChegadas() {
+        int tempoChegada = 0;
+        ArrayList<Integer> listaTemposUsados = new ArrayList<Integer>();
+
+        for (Map.Entry<Integer, Veiculo> veiculo : veiculos.entrySet()) {
+            Veiculo veiculoAtual = veiculo.getValue();
+
+            Random random = new Random();
+            tempoChegada += random.nextInt(getIntervaloChegada());
+
+            while (listaTemposUsados.contains(tempoChegada)) {
+                tempoChegada += 1;
+            }
+            listaTemposUsados.add(tempoChegada);
+
+            temposEventos.add(tempoChegada);
+            Chegada eventoAtual = new Chegada(tempoChegada, veiculoAtual.getIdVeiculo());
+            filaEventos.add(eventoAtual);
+        }
     }
 
-    private Veiculo getVeiculo(int idVeiculo) {
-        return veiculos.get(idVeiculo);
+    private void enfileirarNaCabine(int idCabine, int idVeiculo) {
+        Cabine cabineAtual = getCabine(idCabine);
+        cabineAtual.enfileirarVeiculo(idVeiculo);
+    }
+
+    private void enfileirarSaida(int tempoChegada, int idCabine) {
+        temposEventos.add(tempoChegada);
+        filaEventos.add(new Saida(tempoChegada, idCabine));
+
+    }
+
+    /**
+    * Método que retira um Evento da fila.
+    * @return Evento - remove o Evento da fila com sucesso.
+    */
+    private Evento desenfilerarEvento() {
+        return filaEventos.remove();
+    }
+
+    private void atualizaMediaFilas() {
+        for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
+            Cabine cabineAtual = cabine.getValue();
+            cabineAtual.adicionaNovoTamanho(cabineAtual.calcularTamanho());
+        }
+    }
+
+    private void setTempoTotalSimulado(int tempo) {
+        tempoTotalSimulado = tempo;
     }
 
     /**
@@ -422,104 +520,6 @@ public class Simulador {
         }
 
         return tempoTotal;
-    }
-
-    /**
-    * Método que executa a Saída.
-    * Assim que executo o método, retiro o elemento da fila.
-    * @param e - evento de saida.
-    */
-    private int executarSaida(Saida eventoAtual) {
-        int idVeiculoRemovido = -1;
-        try {
-            Cabine cabineParaRemover = getCabine(eventoAtual.getIdCabine());
-            idVeiculoRemovido = cabineParaRemover.desenfileirarVeiculo();
-            Veiculo veiculoRemovido = getVeiculo(idVeiculoRemovido);
-
-            String tipoVeiculo = (veiculoRemovido instanceof VeiculoLeve) ? "Leve" : "Pesado";
-            cabineParaRemover.armazenaTempo(tipoVeiculo, veiculoRemovido.getTempoEspera());
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return idVeiculoRemovido;
-    }
-
-    /**
-    * Método que gera as Estatisticas da fila de Pegágio.
-    * @return String - retorna a saída final da fila.
-    */
-    public String gerarEstatisticas() {
-        String texto;
-        String textoMaioresTamanhos = getTextoTamanhos("Maior");
-        String textoMedioTamanhos = getTextoTamanhos("Medio");
-        String textoMedioAtendimentosTotal = getTextoMedioAtendimentos("Total");
-        String textoMedioAtendimentosEspecífico = "";
-        textoMedioAtendimentosEspecífico += getTextoMedioAtendimentos("Leve");
-        textoMedioAtendimentosEspecífico += getTextoMedioAtendimentos("Pesado");
-
-        texto = String.format("Tempo total de simulacao: %d\n\n", getTempoTotalSimulado()) +
-                String.format("Numero de eventos tradados: %d\n\n", getNumeroEventosTratados()) +
-                String.format("Tempos médios de espera em cada fila de atendimento:\n\n%s", textoMedioAtendimentosTotal) +
-                String.format("Tamanho médio das filas de atendimento:\n\n%s", textoMedioTamanhos) +
-                String.format("Tamanho máximo das filas de atendimento:\n\n%s", textoMaioresTamanhos) +
-                String.format("Tempo médio de atendimento de cada tipo de cliente:\n\n%s", textoMedioAtendimentosEspecífico);
-
-        return texto;
-    }
-
-    public String getTextoMedioAtendimentos(String abordagem) {
-        String texto = "";
-        int idCabineAtual = 0;
-        Cabine cabineAtual = null;
-        int tempo = 0;
-
-        if (abordagem.equals("Leve")) {
-            texto += "Média de tempo de espera de Veículos Leves:\n";
-        }
-        else if (abordagem.equals("Pesado")) {
-            texto += "Média de tempo de espera de Veículos Pesados:\n";
-        }
-
-        for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
-            idCabineAtual = cabine.getKey();
-            tempo = getTempoMedioEsperaGeral(idCabineAtual, abordagem);
-
-            texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tempo);
-        }
-        return texto + "\n";
-    }
-
-    public String getTextoTamanhos(String abordagem) {
-        String texto = "";
-        int idCabineAtual = 0;
-        int tamanho = 0;
-
-        if (abordagem.equals("Maior")) {
-            for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
-                idCabineAtual = cabine.getKey();
-                tamanho = getTamanhoMaiorFila(idCabineAtual);
-
-                texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tamanho);
-            }
-        }
-        else { // if (abordagem.equals("Medio") {
-            for (Map.Entry<Integer, Cabine> cabine : cabines.entrySet()) {
-                idCabineAtual = cabine.getKey();
-                tamanho = getTamanhoMedioFilaGeral(idCabineAtual);
-
-                texto += String.format("Fila da Cabine %d: %d\n", idCabineAtual, tamanho);
-            }
-        }
-        return texto + "\n";
-    }
-
-    /**
-    * Método que calcula o tempo gasto na execução.
-    * @return long - retorna o tempo gasto na execução.
-    */
-    public long calcularTempoExecucao() {
-        return 0L;
     }
 
     /**
